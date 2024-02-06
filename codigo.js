f3169bcf750b09fb5f29c5398207544d7542b84d
JuanRodriguezSilva22 <153485323+JuanRodriguezSilva22@users.noreply.github.com>
2024-02-06 16:19:04 -0500

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -5,12 +5,20 @@ function irArriba(pxPantalla) {
         var botonArriba = document.getElementById('botonArriba');
 
         if (scroll > pxPantalla) {
-            botonArriba.style.right = 20 + "px";
+            botonArriba.style.right = 30 + "px";
         } else {
             botonArriba.style.right = -100 + "px";
         }
     })
 }
 
-irArriba(200);
 
+irArriba(200);
+/* tomar las 2 variables para menu responsive */
+const toggleMenuElement = document.getElementById('toggle-menu');
+const MenuElement = document.getElementById('menu');
+/*agregar las variables a la clase menu show creada en css*/
+toggleMenuElement.addEventListener('click', () => {
+    MenuElement.classList.toggle('menu--show');
+
+});
